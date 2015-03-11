9f5a9b52a47489e29def4039ae29af8a467e94a5
Charlene Lopez Taw <tawcharlene@gmail.com>
2015-03-11 17:00:50 +0800

    routes

--- app/controllers/api/routes_controller.rb
+++ app/controllers/api/routes_controller.rb
@@ -2,7 +2,7 @@ class Api::RoutesController < ApplicationController
   respond_to :xml, :json
 
   def index 
-    respond_with(@routes = Route.select("start_name, end_name, no_of_way").order("id DESC"))
+    respond_with(@routes = Route.all)
   end
   
 end
