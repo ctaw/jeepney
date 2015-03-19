7d33b62ef9cb66c3c94c45e9a49258712b32b35f
Charlene Lopez Taw <tawcharlene@gmail.com>
2015-03-20 07:48:46 +0800

    order list

--- app/controllers/api/fares_controller.rb
+++ app/controllers/api/fares_controller.rb
@@ -2,7 +2,7 @@ class Api::FaresController < ApplicationController
   respond_to :xml, :json
 
   def index 
-    respond_with(@fares = Fare.all)
+    respond_with(@fares = Fare.all.order("id ASC"))
   end
 
 end
