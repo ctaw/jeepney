95bc8e66fd957fdb2e927b185b608e94999c70df
Charlene Lopez Taw <tawcharlene@gmail.com>
2015-03-20 16:52:18 +0800

    jeepney routes

--- app/controllers/api/jeepney_routes_controller.rb
+++ app/controllers/api/jeepney_routes_controller.rb
@@ -2,11 +2,12 @@ class Api::JeepneyRoutesController < ApplicationController
   respond_to :xml, :json
 
   def index 
-    respond_with(@jeepney_routes = JeepneyRoute.all)
+    respond_with(@jeepney_routes = JeepneyRoute.select("*").joins(:route))
   end
 
   def show
-    respond_with(@jeepney_routes = JeepneyRoute.where("route_id =?", params[:id])) 
+    # respond_with(@jeepney_routes = JeepneyRoute.where("route_id =?", params[:id])) 
+    respond_with(@jeepney_routes = JeepneyRoute.select("*").joins(:route).where(:jeepney_routes => {:route_id => params[:id]}))
   end
   
 end
